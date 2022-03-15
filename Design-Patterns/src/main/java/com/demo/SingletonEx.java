2a84640997d00ff5ba1edf30236dbdd3239dcbd6
Sanjeev Komma <sanjeevkomma@gmail.com>
2022-03-16 00:06:35 +0530

    in

---
+++ Design-Patterns/src/main/java/com/demo/SingletonEx.java
@@ -0,0 +1,32 @@
+package com.demo;
+
+//Java code to explain double check locking
+public class SingletonEx
+{
+	// private instance, so that it can be
+	// accessed by only by getInstance() method
+	private static SingletonEx instance;
+
+	private SingletonEx()
+	{
+		// private constructor
+	}
+
+	public static SingletonEx getInstance()
+	{
+		if (instance == null)
+		{
+			//synchronized block to remove overhead
+			synchronized (SingletonEx.class)
+			{
+				if(instance==null)
+				{
+					// if instance is null, initialize
+					instance = new SingletonEx();
+				}
+
+			}
+		}
+		return instance;
+	}
+}
